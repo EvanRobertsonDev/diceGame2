837848cfa0ab951ab11ef971ed5887f663637e8e
Evan Robertson <evan-robertson@outlook.com>
2021-01-30 19:30:20 -0500

    Third commit

--- src/dicegame/DiceGame.java
+++ src/dicegame/DiceGame.java
@@ -20,37 +20,56 @@ public class DiceGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Input
         Scanner input = new Scanner(System.in);
+        
+        //Constants
         final int DIE1_SIDES = 6;
         final int DIE2_SIDES = 6;
         final int MAX_ROLLS = 10;
         
+        //Score variables
         int compScore = 0;
         int userScore = 0;
         
+        //Create dice objects
         die die1 = new die(DIE1_SIDES);
         die die2 = new die(DIE2_SIDES);
         
+        //Get name of computer
         System.out.println("Enter the name of the computer");
         while (die1.getComputerName() == null) {
             die1.setComputerName(input.nextLine());
         }
+        
+        //Get name of user
         System.out.println("Enter the name of the user");
         while (die2.getUserName() == null) {
             die2.setUserName(input.nextLine());
         }
+        
+        //Print info
         System.out.println("This program simulates rolling a " + DIE1_SIDES + " sided die and a " + DIE2_SIDES + " sided die");
         System.out.println("Rolling the dice" + MAX_ROLLS + " times...");
         System.out.println("Die1  Die2");
+        
+        //Loop for 10 rolls
         for(int i = 0; i < MAX_ROLLS; i++) {
+            
             die1.roll();
             die2.roll();
+            
+            //Increase scores
             if (die1.getValue() > die2.getValue()) {
                 compScore++;
             }
+            
             else if (die1.getValue() < die2.getValue()) {
                 userScore++;
             }
+            
+            //Print results
             System.out.println(die1.getValue() + "     " + die2.getValue());
         }
         //Print score
